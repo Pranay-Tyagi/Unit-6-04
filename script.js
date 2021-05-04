2135886e07288f61bc143737c3edfa3ac8b50fd1
PranayTyagi1 <>
2021-05-04 13:28:27 +0000

    added let

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
+
 document.querySelector('.calculate').addEventListener('click', function () {
-  a = document.querySelector('.a').value
-  b = document.querySelector('.b').value
-  height = document.querySelector('.height').value
+  let a = document.querySelector('.a').value
+  let b = document.querySelector('.b').value
+  let height = document.querySelector('.height').value
   a = parseInt(a)
   b = parseInt(b)
   height = parseInt(height)
-  area = (a + b) / 2 * height
+  let area = (a + b) / 2 * height
   alert(area)
 })
