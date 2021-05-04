8b1bcb601285f809e62413e1d42ed6454358d896
PranayTyagi1 <>
2021-05-04 13:21:26 +0000

    positioning and spacing

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('.calculate').addEventListener('click', function () {
   a = document.querySelector('.a').value
-  a = parseInt(a)
   b = document.querySelector('.b').value
-  b = parseInt(b)
   height = document.querySelector('.height').value
+  a = parseInt(a)
+  b = parseInt(b)
   height = parseInt(height)
-  area = ((a+b)/2)*height
+  area = (a + b) / 2 * height
   alert(area)
 })
