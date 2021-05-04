6258c4dfe342159f584aa7e5a6ddb4fda7468e82
PranayTyagi1 <>
2021-05-04 13:57:34 +0000

    float for decimals

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.querySelector('.calculate').addEventListener('click', function () {
   let a = document.querySelector('.a').value
   let b = document.querySelector('.b').value
   let height = document.querySelector('.height').value
-  a = parseInt(a)
-  b = parseInt(b)
-  height = parseInt(height)
+  a = parseFloat(a)
+  b = parseFloat(b)
+  height = parseFloat(height)
   const area = (a + b) / 2 * height
   alert(area)
 })
